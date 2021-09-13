55438ffbded0209ed8b6760d48cc575fec1b1e24
Abhishek <dhanani9638a@gmail.com>
2021-09-13 20:27:45 +0530

    create auth and signup api

---
+++ src/auth/user.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    userName: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+}
