5c57096224b6e2521c9b3d802b28d7de4bec762a
Abhishek <dhanani9638a@gmail.com>
2021-09-13 19:10:17 +0530

    add costom error

--- src/tasks/dto/update-task-status.dto.ts
+++ src/tasks/dto/update-task-status.dto.ts
@@ -2,7 +2,9 @@ import { IsEnum } from "class-validator";
 import { TaskStatus } from "../task.model";
 
 
-export class UpdateTaskStatusDTO{
-    @IsEnum(TaskStatus)
+export class UpdateTaskStatusDTO {
+    @IsEnum(TaskStatus, {
+        message: `Must Be ${TaskStatus.OPEN}, ${TaskStatus.IN_PROGRESS} or ${TaskStatus.DONE}`,
+    })
     status: TaskStatus;
 }
