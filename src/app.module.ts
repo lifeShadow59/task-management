908b9c0f4b6e9cc45151b595f0c36aa796c62ae1
lifeshadow <abhishek.dhanani@bosctechlabs.com>
2021-09-14 15:22:58 +0530

    create auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { configuration } from './configuration/orm.configuration';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TasksModule, TypeOrmModule.forRoot(configuration)],
+  imports: [TasksModule, TypeOrmModule.forRoot(configuration), AuthModule],
 })
 export class AppModule {}
